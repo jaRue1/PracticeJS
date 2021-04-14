4fb339974ee1dd46f470fc423fd80020b7d9dc6e
jaRue1 <jaruejohnson@outlook.com>
2021-04-14 00:21:07 -0400

    uncommeted a console.log statement

--- BASICS/templateLiterals.js
+++ BASICS/templateLiterals.js
@@ -5,4 +5,4 @@ console.log(`Hello My Name is ${firstName}. I'm from ${homeTown}.`)
 
 // Combine Ternary and Template Literal
 let str =  `How are you doing ${(firstName) ? firstName : 'No Name'}`
-//console.log(str)
+console.log(str)
