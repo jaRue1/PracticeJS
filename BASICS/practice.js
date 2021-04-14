e2fcd7c330b8c82f093511c3a6b2f82b49f13388
jaRue1 <jaruejohnson@outlook.com>
2021-04-14 00:19:52 -0400

    added my first practice file

---
+++ BASICS/practice.js
@@ -0,0 +1,14 @@
+// Create a variable its either a let or a const add a variable name then assign a value
+let age = 23
+
+// create a string
+const address = "1483 Running Oak Lane 33411 Royal Palm Beach Fl"
+
+// create a function do something or return something
+function myInfo(x, y){
+let myAge = x 
+let myAddress = y
+console.log("My age is " + myAge + " My address is "+  myAddress)
+}
+// this is how i call a function
+myInfo(age,address)
