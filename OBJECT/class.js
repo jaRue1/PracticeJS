58be01dccfcdfde5fe0740dcffcd89b93c308f0a
jaRue1 <jaruejohnson@outlook.com>
2021-04-17 21:09:51 -0400

    updated my personal example still showing an undefined error will troubleshoot later

--- OBJECT/class.js
+++ OBJECT/class.js
@@ -1,23 +1,35 @@
 class God{
-  constructor(firstName,soul,time,domain){
-    this.firstName = firstName
-    this.soul = soul
-    this.time = time
-    this.domain = domain
+  constructor(creator){
+    this.firstName = creator
+    
   }
-  believe(){
+  promise(){
     return `You are a child of ${this.firstName}`
   }
+  believe(){
+    return `Any thing is possible for those that believe `
+  }
+
 }
 class Man extends God{
-  constructor(gender,height,weight,race){
-    super(firstName,soul,time,domain)
+  constructor(creator, gender,height,weight,ethnicity){
+    super(creator)
     this.gender = gender
     this.height = height
     this.weight = weight
-    this.race = race
+    this.race = ethnicity
   }
   gift(){
-    console.log(this.believe() + ' and the ' + this.race + 'race has dominion over the ' + this.domain)
+    console.log(`${this.believe()} Don't stress  ${this.firstName}  has a plan for you ` )
   }
 }
+
+let human = new Man('God','Man',"6'5",'190 lbs','Black')
+console.log('----------- When things get tough REMEMBER this-------------')
+console.log(human.promise())
+console.log(human.gift())
+console.log('========== HUMAN ATTRIBUTES ==========')
+console.log("I am ",human.gender)
+console.log("I'm a lightweight I only weigh ",human.weight)
+console.log("I am pretty short I am ",human.height, "inches")
+console.log("Say it loud I am ",human.race, " and I am proud ! ")
