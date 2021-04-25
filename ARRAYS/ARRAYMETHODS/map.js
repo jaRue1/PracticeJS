7b92032e6c0b26ef7ef63d4293381ba7fa1b8b1f
jaRue1 <jaruejohnson@outlook.com>
2021-04-24 21:25:03 -0400

    added some files to my array methods folder

---
+++ ARRAYS/ARRAYMETHODS/map.js
@@ -0,0 +1,4 @@
+const jrDev = ['HTML','CSS','JS','PYTHON','SQL','NOSQL','REACT']
+const midDev = [jrDev,'DOCKER','API','NEXTJS','PANDAS']
+const srDev = [jrDev,midDev,'AZURE','AWS','C#','JAVA','GO']
+const BocaCode = [jrDev,midDev,srDev]
