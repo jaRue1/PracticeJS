8118a2cc0cc0cf44bfac37e6dde8425190166e4d
jaRue1 <jaruejohnson@outlook.com>
2021-04-26 21:05:53 -0400

    added a example with reverse

--- ARRAYS/ARRAYMETHODS/reverse.js
+++ ARRAYS/ARRAYMETHODS/reverse.js
@@ -1,1 +1,34 @@
- 
+ const nbaRoster = [
+  {
+    playerTeam: "Lakers",
+    playerName: "Lebron James",
+    playerHeight: "6'8",
+    playerWeight: "240lbs",
+    playerPos: "SF",
+  },
+  {
+    playerTeam: "Nets",
+    playerName: "Kevin Durant",
+    playerHeight: "6'11",
+    playerWeight: "230lbs",
+    playerPos: "SF",
+  },
+  {
+    playerTeam: "Clippers",
+    playerName: "Paul George",
+    playerHeight: "6'8",
+    playerWeight: "225lbs",
+    playerPos: "SF",
+  },
+  {
+    playerTeam: "Trailblazer",
+    playerName: "Damian Lilard",
+    playerHeight: "6'2",
+    playerWeight: "200lbs",
+    playerPos: "PG",
+  },
+]
+nbaRoster.reverse() // reverses the order of the array
+nbaRoster.forEach(player => {
+  console.log(player.playerTeam, player.playerName, player.playerPos) 
+});
