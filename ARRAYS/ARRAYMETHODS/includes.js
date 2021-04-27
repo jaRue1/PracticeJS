17375f2739aeffbcd95493a07a57a387efd77f34
jaRue1 <jaruejohnson@outlook.com>
2021-04-26 21:02:28 -0400

    created a includes arraymethod example

---
+++ ARRAYS/ARRAYMETHODS/includes.js
@@ -0,0 +1,8 @@
+//  The includes() method determines whether an array contains a specified element.
+// This method returns true if the array contains the element, and false if not
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"]
+let n = fruits.includes("Mango")
+let y = fruits.includes("Peach")
+console.log(n)// true
+console.log(y) // false
