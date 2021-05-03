a5d23f0b09526430d7c0e1dccde38f2b54e4c30e
jaRue1 <jaruejohnson@outlook.com>
2021-05-03 17:51:02 -0400

    added a math folder

---
+++ MATH/random.js
@@ -0,0 +1,21 @@
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
+}
+let myArray = [1,2,3,4,5,6,7,8,9,10]
+console.log(shuffle(myArray))
---
+++ MATH/randomv2.js
@@ -0,0 +1,9 @@
+let copy = (array) => {
+  let x = array.length 
+  let i; 
+  
+
+  return output 
+
+}
+let myArray = [1,2,3,4,5,6,7,8,9,10]
