e8ee7195c0787a365064a4b5e919ce815b35ae92
jaRue1 <jaruejohnson@outlook.com>
2023-12-22 22:32:20 -0500

    fixed one

--- BASICS/animalSounds.js
+++ BASICS/animalSounds.js
@@ -1,19 +1,16 @@
-function animalSound(animalName){
-  let noise
-if (animalName === 'cat'){
-  return 'Meow'
+function animalSound(animalName) {
+  if (animalName === "cat") {
+    return "Meow"
+  }
+  if (animalName === "cow") {
+    return "Moo"
+  }
+  if (animalName === "dog") {
+    return "Woof"
+  } else {
+    return "merp"
+  }
 }
-if (animalName === 'cow'){
-  return 'Moo'
-}
-if (animalName === 'dog'){
-  return 'Woof'
-}
-else{
-  return 'merp'
-}
-return noise
-}
-let animal = 'cat'
+let animal = "cat"
 let animalSounds = animalSound(animal)
-console.log(animalSounds);
+console.log(animalSounds)
