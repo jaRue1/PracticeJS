0fbb104aee9b7b0c2c7707b22e603f2619fe2216
jaRue1 <jaruejohnson@outlook.com>
2023-12-22 22:44:06 -0500

    fixed seven

--- FUNCS/bAFunc.js
+++ FUNCS/bAFunc.js
@@ -1,11 +1,13 @@
 // write a function using big arrow notation
-let num1 = 2
-let num2 = 3
-// constant variable Sum assigned the value of the anonymous function 
+
+// constant variable sum assigned the value of the anonymous function
 // big arrow notation is written as follows
 // (( Parameters ) => { function })
-const Sum = ((x,y) =>  {
-  let result = x + y
-  return result
-})
-console.log("This is big arrow notation :  Sum = " + Sum(num1,num2))
+
+let num1 = 2
+let num2 = 3
+
+const sum = (x, y) => {
+  return x + y
+}
+console.log("This is big arrow notation :  Sum = " + sum(num1, num2))
