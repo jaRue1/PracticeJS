77e4c4c57768f22ac6f2a167441da89fdb109bdf
jaRue1 <jaruejohnson@outlook.com>
2023-12-22 22:34:26 -0500

    fixed three

--- BASICS/functions.js
+++ BASICS/functions.js
@@ -1,13 +1,14 @@
-
-// standard function 
+// standard function
 function calcTax(total, taxRate) {
-  const tax = total * taxRate
-return tax
- } 
-let myTax = calcTax(7.25, 0.07) 
+  return total * taxRate
+}
+let myTax = calcTax(7.25, 0.07)
 
-
-const square = function(num) { return num * num } // anonymous function
-const square1 = (num) => { return num * num } // big arrow notation 
+const square = function (num) {
+  return num * num
+} // anonymous function
+const square1 = (num) => {
+  return num * num
+} // big arrow notation
 
 let x = square(4) // this should equal 16
