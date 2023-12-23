e59483444bc85d102f8f9cfa21c689952c341b14
jaRue1 <jaruejohnson@outlook.com>
2023-12-22 22:47:31 -0500

    fixed nine

--- OBJECT/class.js
+++ OBJECT/class.js
@@ -1,35 +1,36 @@
-class God{
-  constructor(creator){
+class God {
+  constructor(creator) {
     this.firstName = creator
-    
   }
-  promises(){
+  promises() {
     return `You are a child of ${this.firstName}`
   }
-  believe(){
+  believe() {
     return `Any thing is possible for those that believe `
   }
-
 }
-class Man extends God{
-  constructor(creator, gender,height,weight,ethnicity){
+class Man extends God {
+  constructor(creator, gender, height, weight, ethnicity) {
     super(creator)
     this.gender = gender
     this.height = height
     this.weight = weight
     this.race = ethnicity
   }
-  gift(){
-    return `${this.believe()} Don't stress  ${this.firstName}  has a plan for you `
+  gift() {
+    return `${this.believe()} Don't stress  ${
+      this.firstName
+    }  has a plan for you `
   }
 }
 
-let human = new Man('God','Man',"6'5",'190 lbs','Black')
-console.log('----------- When things get tough REMEMBER this-------------')
-console.log(human.promises())
-console.log(human.gift())
-console.log('========== HUMAN ATTRIBUTES ==========')
-console.log("I am ",human.gender)
-console.log("I'm a lightweight I only weigh ",human.weight)
-console.log("I am pretty short I am ",human.height, "inches")
-console.log("Say it loud I am ",human.race, " and I am proud ! ")
+let man = new Man("God", "Man", "6'5", "190 lbs", "Black")
+
+console.log("----------- When things get tough REMEMBER this-------------")
+console.log(man.promises())
+console.log(man.gift())
+console.log("========== HUMAN ATTRIBUTES ==========")
+console.log("I am ", man.gender)
+console.log("I'm a lightweight I only weigh ", man.weight)
+console.log("I am pretty short I am ", man.height, "inches")
+console.log("Say it loud I am ", man.race, " and I am proud ! ")
